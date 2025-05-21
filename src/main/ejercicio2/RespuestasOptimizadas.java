3d511256586d7cd0170d585cd5113a8f97c19546
Daniel Sanchez <166523536+01010101010101011010010101101@users.noreply.github.com>
2025-05-20 19:54:51 -0500

    actualizacion project

--- src/main/ejercicio2/RespuestasOptimizadas.java
+++ src/main/ejercicio2/RespuestasOptimizadas.java
@@ -1,30 +1,48 @@
 package main.ejercicio2;
 
-public class RespuestasOptimizadas {
-    // El estudiante debe escribir aquí las versiones optimizadas y las
-    // complejidades
+import java.util.HashSet;
 
-    // 1. Versión optimizada de tieneDuplicados (Complejidad anotada en la variable)
-    public static String tieneDuplicadosComplejidad = ""; // Ej: "O(n)"
+public class RespuestasOptimizadas {
+
+    // 1. Buscar duplicados en un arreglo
+    public static String tieneDuplicadosComplejidad = "O(n)";
 
     public static boolean tieneDuplicadosOpt(int[] arr) {
-        // TODO: implementar versión eficiente
-        return false;
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            if (set.contains(num)) {
+                return true; // Duplicado encontrado
+            }
+            set.add(num);
+        }
+        return false; // No hay duplicados
     }
 
-    // 2. Versión optimizada de contarMayores (Complejidad anotada en la variable)
-    public static String contarMayoresComplejidad = ""; // Ej: "O(n)"
+    // 2. Contar elementos mayores que X
+    public static String contarMayoresComplejidad = "O(n)";
 
     public static int contarMayoresOpt(int[] arr, int x) {
-        // TODO: implementar versión eficiente
-        return 0;
+        int contador = 0;
+        for (int num : arr) {
+            if (num > x) { 
+                contador++;
+            }
+        }
+        return contador;
     }
 
-    // 3. Versión optimizada de encontrarMaximo (Complejidad anotada en la variable)
-    public static String encontrarMaximoComplejidad = ""; // Ej: "O(n)"
+    // 3. Encontrar el valor máximo en un arreglo
+    public static String encontrarMaximoComplejidad = "O(n)";
 
     public static int encontrarMaximoOpt(int[] arr) {
-        // TODO: implementar versión eficiente
-        return 0;
+        int max = Integer.MIN_VALUE;
+        for (int num : arr) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
     }
 }
+
+
